7f0ed375bcd9b6482586501c45d7f23780e4aa98
Alex <alexcc17@gmail.com>
2019-05-07 12:11:58 -0400

    Create files

---
+++ FindACrew/Model Controllers/PersonController.swift
@@ -0,0 +1,9 @@
+//
+//  PersonController.swift
+//  FindACrew
+//
+//  Created by Alex on 5/7/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import Foundation
---
+++ FindACrew/Views/PersonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonTableViewCell.swift
+//  FindACrew
+//
+//  Created by Alex on 5/7/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
